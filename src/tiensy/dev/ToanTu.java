0ca8dd76700f0eae16e859422ec0f4e89824186b
vansy0147 <syvan190900>
2018-10-06 15:29:58 +0700

    first commit

---
+++ src/tiensy/dev/ToanTu.java
@@ -0,0 +1,38 @@
+package tiensy.dev;
+
+public class ToanTu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int tong = a+b;
+		int hieu = a-b;
+		int tich = a*b;
+		int thuong = a/b;
+		int phandu = a%b;
+		System.out.println(tong);
+		System.out.println(hieu);
+		System.out.println(tich);
+		System.out.println(thuong);
+		System.out.println(phandu);
+		a++;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a*=b);
+		System.out.println(a+=b);
+		System.out.println(a-=b);
+		System.out.println(a/=b);
+		
+		
+
+	}
+
+}
